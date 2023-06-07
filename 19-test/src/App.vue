8007a5a4edf509d36a2fece68bce6ddb94e76afe
TroyTrojan <319635848@qq.com>
2023-06-07 21:34:22 +0800

    0000

---
+++ 19-test/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="demo">欢迎你</div>
+    <school />
+    <student />
+  </div>
+</template>
+
+<script>
+import school from "./components/school.vue";
+import student from "./components/student.vue";
+export default {
+  name: "App",
+  components: {
+    school,
+    student,
+  },
+};
+</script>
+
+<style scoped>
+/* scope只对子文件起作用,对App.vue不起作用 */
+.demo {
+  color: red;
+}
+</style>
---
+++ 19-test/src/components/school.vue
@@ -0,0 +1,33 @@
+  <!-- 组件结构 -->
+<template>
+  <div class="demo">
+    <h4 @click="show">学校:{{ name }}</h4>
+    <h4>地址:{{ address }}</h4>
+  </div>
+</template>
+<script>
+import { hunru, hunru2 } from "../aaa";
+// 组建的交互数据和方法
+export default {
+  name: "school",
+  data() {
+    return {
+      name: "山东商务",
+      address: "烟台",
+      a: 8888,
+    };
+  },
+  methods: {
+    show() {
+      alert(this.name);
+    },
+  },
+  mixins: [hunru, hunru2],
+};
+</script>
+<style scoped>
+/* 组件的样式 */
+.demo {
+  background-color: blue;
+}
+</style>
